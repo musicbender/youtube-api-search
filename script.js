808d4b1ac216fb83abd8cbe921eb9be4708021af
musicbender <musicbender@gmail.com>
2015-12-14 14:31:23 -0800

    Sucessfully requested and displayed youtube api data text. Began styling as well

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 $(document).ready(function(){
     
-    function getRequest () {
+    $('.search-form').submit(function(){
+        event.preventDefault();
+        var input = $('.form-input').val();
+        getRequest(input);
+    });
+    
+    function getRequest (searchTerm) {
         var params = {
-            
+            part: "snippet",
+            key: 'AIzaSyDwbooupkorSaCDsUHfwnnMQlLZDhNRyzM',	
+            q: searchTerm
         }
+        
+        var url = 'https://www.googleapis.com/youtube/v3/search';
+        
+        $.getJSON(url, params, function(data){
+            console.log(data.items);
+            showResults(data.items);
+        });
+    }
+    
+    function showResults(results) {
+        var html = "";
+        $.each(results, function(index, value){
+            html += '<li class="item">' + value.snippet.title + '</li>';
+            $('.results').html(html);
+            
+        });
     }
     
 });
